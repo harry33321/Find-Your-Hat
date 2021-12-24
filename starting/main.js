f67c287c14425befd79c8de6722bc101b7feaac6
Harry Lam <harry33321@gmail.com>
2021-12-24 14:40:16 +0800

    4 class

--- starting/main.js
+++ starting/main.js
@@ -1,15 +1,27 @@
 const prompt = require('prompt-sync')({ sigint: true });
 
-class Field {
+class Character {
   constructor() {
     this.hat = '^';
     this.hole = 'O';
     this.fieldCharacter = '░';
     this.pathCharacter = '*';
-    this.fieldArray = [];
+  }
+}
+
+class Position {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+class FindYourHat {
+  constructor(fieldArray) {
+    this.char = new Character()
+    this.pos = new Position(0, 0)
+    this.fieldArray = fieldArray;
     this.playing = true;
-    this.x = 0;
-    this.y = 0;
     this.hardMode = false;
   }
 
@@ -32,29 +44,29 @@ class Field {
   createChar() {
     let hatY = Math.floor(Math.random() * this.fieldArray.length);
     let hatX = Math.floor(Math.random() * this.fieldArray[hatY].length);
-    this.fieldArray[hatY][hatX] = this.hat;
+    this.fieldArray[hatY][hatX] = this.char.hat;
 
     let pathY = Math.floor(Math.random() * this.fieldArray.length);
     let pathX = Math.floor(Math.random() * this.fieldArray[pathY].length);
-    this.x = pathX;
-    this.y = pathY;
-    this.fieldArray[pathY][pathX] = this.pathCharacter;
+    this.pos.x = pathX;
+    this.pos.y = pathY;
+    this.fieldArray[pathY][pathX] = this.char.pathCharacter;
   }
 
   move() {
     let way = prompt('Which way? ');
     switch (way) {
       case "w":
-        this.y -= 1;
+        this.pos.y -= 1;
         break;
       case "s":
-        this.y += 1;
+        this.pos.y += 1;
         break;
       case "a":
-        this.x -= 1;
+        this.pos.x -= 1;
         break;
       case "d":
-        this.x += 1;
+        this.pos.x += 1;
         break;
       default:
         console.log("Wrong Input, end the game!");
@@ -68,38 +80,51 @@ class Field {
   hardMove() {
     let randomYIndex = Math.floor(Math.random() * this.fieldArray.length);
     let randomXIndex = Math.floor(Math.random() * this.fieldArray[randomYIndex].length);
-    while (this.fieldArray[randomYIndex][randomXIndex] == this.hat || this.fieldArray[randomYIndex][randomXIndex] == this.hole) {
+    while (this.fieldArray[randomYIndex][randomXIndex] == this.char.hat || this.fieldArray[randomYIndex][randomXIndex] == this.char.hole) {
       randomYIndex = Math.floor(Math.random() * this.fieldArray.length);
       randomXIndex = Math.floor(Math.random() * this.fieldArray[randomYIndex].length);
     }
-    this.fieldArray[randomYIndex][randomXIndex] = this.hole;
+    this.fieldArray[randomYIndex][randomXIndex] = this.char.hole;
   }
 
   checkWinLoss() {
-    if (this.y < 0 || this.x < 0 || this.y >= this.fieldArray.length || this.x >= this.fieldArray[this.y].length) {
+    if (this.pos.y < 0 || this.pos.x < 0 || this.pos.y >= this.fieldArray.length || this.pos.x >= this.fieldArray[this.pos.y].length) {
       console.log(`Out of bounds instruction`);
       this.playing = false;
-    } else if (this.fieldArray[this.y][this.x] == this.hole) {
+    } else if (this.fieldArray[this.pos.y][this.pos.x] == this.char.hole) {
       console.log(`Sorry, you fell down a hole`);
       this.playing = false;
-    } else if (this.fieldArray[this.y][this.x] == this.hat) {
+    } else if (this.fieldArray[this.pos.y][this.pos.x] == this.char.hat) {
       console.log(`You win, you found your hat`);
       this.playing = false;
-    } else if (this.fieldArray[this.y][this.x] == this.fieldCharacter) {
-      this.fieldArray[this.y][this.x] = this.pathCharacter;
+    } else if (this.fieldArray[this.pos.y][this.pos.x] == this.char.fieldCharacter) {
+      this.fieldArray[this.pos.y][this.pos.x] = this.char.pathCharacter;
     }
   }
+}
 
-  generateField(x, y, percentage) {
-    this.fieldArray = new Array(y).fill(0).map(i => new Array(x))
+class Field {
+  static generateField(x, y, percentage, char) {
+    let mapArray = new Array(y).fill(0).map(i => new Array(x))
     for (let i = 0; i < y; i++) {
       for (let j = 0; j < x; j++) {
         percentage > Math.random() ?
-          this.fieldArray[i][j] = this.fieldCharacter :
-          this.fieldArray[i][j] = this.hole;
+          mapArray[i][j] = char.fieldCharacter :
+          mapArray[i][j] = char.hole;
       }
     }
+    return mapArray
   }
+  // generateField(x, y, percentage) {
+  //   this.fieldArray = new Array(y).fill(0).map(i => new Array(x))
+  //   for (let i = 0; i < y; i++) {
+  //     for (let j = 0; j < x; j++) {
+  //       percentage > Math.random() ?
+  //         this.fieldArray[i][j] = this.fieldCharacter :
+  //         this.fieldArray[i][j] = this.hole;
+  //     }
+  //   }
+  // }
   //   generateField(x, y, percentage) {
   //     const mapObject = [this.hole, this.fieldCharacter];
   //     let checkMap = 0;
@@ -118,6 +143,8 @@ class Field {
   // }
 }
 
-const myField = new Field();
-myField.generateField(6, 4, 0.8);
-myField.playGame();
+const char = new Character();
+const genField = Field.generateField(6, 4, 0.8, char)
+const myGame = new FindYourHat(genField);
+// myField.generateField(6, 4, 0.8);
+myGame.playGame();
